79ec37bcc5515478b34426c390b2c1e662a0385b
AyukGiress <giress865@gmail.com>
2024-02-27 00:00:26 +0100

    editing erros

--- assets/style/index.js
+++ assets/style/index.js
@@ -1,55 +1,54 @@
-const api = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
-const searchButton = document.getElementById('btn');
-const searchInput = document.getElementById('user-value');
-const searchResult = document.getElementById('result');
+const api = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
+const searchInput = document.getElementById('user-value')
+const searchResult = document.getElementById('result')
 
 searchInput.addEventListener('input', function () {
   const searchTerm = searchInput.value;
 
   // Call the debounced search function with the input value
-  debouncedSearch(searchTerm);
-});
+  debouncedSearch(searchTerm)
+})
 
 const debounce = (func, delay) => {
-  let timeoutId;
-  return function() {
-    const context = this;
-    const args = arguments;
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => func.apply(context, args), delay);
-  };
-};
+  let timeoutId
+  return function () {
+    const context = this
+    const args = arguments
+    clearTimeout(timeoutId)
+    timeoutId = setTimeout(() => func.apply(context, args), delay)
+  }
+}
 
-function fetchRecipes(searchTerm) {
+function fetchRecipes (searchTerm) {
   fetch(api + searchTerm)
     .then(response => response.json())
     .then(data => {
-      const recipes = data.meals;
-      displayRecipes(recipes);
+      const recipes = data.meals
+      displayRecipes(recipes)
     })
-    .catch(error => console.error('Error fetching recipes:', error));
+    .catch(error => console.error('Error fetching recipes:', error))
 }
 
-function displayRecipes(recipes) {
-  searchResult.innerHTML = '';
+function displayRecipes (recipes) {
+  searchResult.innerHTML = ''
 
   if (recipes) {
     recipes.forEach(recipe => {
-      const recipeElement = document.createElement('div');
+      const recipeElement = document.createElement('div')
       recipeElement.innerHTML = `
         <h2>${recipe.strMeal}</h2>
         <img src="${recipe.strMealThumb}" alt="${recipe.strMeal}" class="recipe-image">
         <p>${recipe.strInstructions}</p>
-      `;
-      searchResult.appendChild(recipeElement);
-    });
+      `
+      searchResult.appendChild(recipeElement)
+    })
   } else {
-    searchResult.textContent = 'Oops, not found';
+    searchResult.textContent = 'Oops, not found'
   }
 }
 
 const handleSearch = (searchTerm) => {
-  fetchRecipes(searchTerm);
-};
+  fetchRecipes(searchTerm)
+}
 
-const debouncedSearch = debounce(handleSearch, 500); // Debounce the handleSearch function
+const debouncedSearch = debounce(handleSearch, 500) // Debounce the handleSearch function
